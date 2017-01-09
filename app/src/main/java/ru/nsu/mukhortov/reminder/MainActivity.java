4e996c2e1326180532339f1e20d25e7f567dd619
Ivan <vanyaprime@gmail.com>
2017-01-09 23:31:16 +0700

    add cardview

--- app/src/main/java/ru/nsu/mukhortov/reminder/MainActivity.java
+++ app/src/main/java/ru/nsu/mukhortov/reminder/MainActivity.java
@@ -2,12 +2,43 @@ package ru.nsu.mukhortov.reminder;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.view.Menu;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
+    private RecyclerView mRecyclerView;
+    private RecyclerView.Adapter mAdapter;
+    private RecyclerView.LayoutManager mLayoutManager;
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_items, menu);
+        return true;
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mRecyclerView = (RecyclerView)findViewById(R.id.my_recycler_view);
+        mLayoutManager = new LinearLayoutManager(this);
+        mRecyclerView.setLayoutManager(mLayoutManager);
+        ArrayList<String> myDataset = new ArrayList<>();
+        myDataset.add("1 task");
+        myDataset.add("2 task");
+        myDataset.add("3 task");
+        myDataset.add("4 task");
+        myDataset.add("5 task");
+        myDataset.add("6 task");
+
+
+        mAdapter = new TaskAdapter(myDataset);
+        mRecyclerView.setAdapter(mAdapter);
+
     }
+
+
 }
