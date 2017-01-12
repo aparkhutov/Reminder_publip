54d74be88d8b145043f8b2627043743b928e0ea5
Ivan Permyakov <iapermyakov@gmail.com>
2017-01-13 00:13:45 +0700

    little fix

--- app/src/main/java/ru/nsu/mukhortov/reminder/MainActivity.java
+++ app/src/main/java/ru/nsu/mukhortov/reminder/MainActivity.java
@@ -10,6 +10,7 @@ import ru.nsu.mukhortov.TasksAdapter;
 import ru.nsu.mukhortov.reminder.model.Task;
 
 import java.util.Arrays;
+import java.util.List;
 
 @EActivity(R.layout.activity_main)
 public class MainActivity extends AppCompatActivity {
@@ -19,11 +20,19 @@ public class MainActivity extends AppCompatActivity {
 
     @AfterViews
     void initListView() {
-        Task task = new Task("Батя", "В здании");
-        Task task1 = new Task("Батя", "В здании");
-        Task task2 = new Task("Батя", "В здании");
-        Task task3 = new Task("Батя", "В здании");
-        tasksAdapter = new TasksAdapter(this, R.layout.task_item, Arrays.asList(task, task1, task2, task3));
+        List<Task> tasks = Task.listAll(Task.class);
+        if (tasks.size() == 0) {
+            Task task = new Task("Батя", "В здании");
+            Task task1 = new Task("Батя", "В здании1");
+            Task task2 = new Task("Батя", "В здании2");
+            Task task3 = new Task("Батя", "В здании3");
+            task.save();
+            task1.save();
+            task2.save();
+            task3.save();
+            tasks = Arrays.asList(task, task1, task2, task3);
+        }
+        tasksAdapter = new TasksAdapter(this, R.layout.task_item, tasks);
         listView.setAdapter(tasksAdapter);
     }
 }
